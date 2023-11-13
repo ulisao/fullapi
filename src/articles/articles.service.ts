20b1f4a13d75222d0998ffdcd08ab4e01f3ad180
ulisao <ulisessbaretta@gmail.com>
2023-11-13 14:33:46 -0300

    Articles Api CRUD finished

---
+++ src/articles/articles.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { CreateArticleDto } from './dto/create-article.dto';
+import { UpdateArticleDto } from './dto/update-article.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class ArticlesService {
+  constructor(private prisma: PrismaService) {}
+
+  create(createArticleDto: CreateArticleDto) {
+    //This action adds a new article;
+    return this.prisma.article.create({ data: createArticleDto });
+  }
+
+  findAll() {
+    //This action returns all articles;
+    return this.prisma.article.findMany({ where: { published: true } });
+  }
+
+  findDrafts() {
+    return this.prisma.article.findMany({ where: { published: false } });
+  }
+
+  findOne(id: number) {
+    //This action returns a #${id} article
+    return this.prisma.article.findUnique({ where: { id } });
+  }
+
+  update(id: number, updateArticleDto: UpdateArticleDto) {
+    //This action updates a #${id} article;
+    return this.prisma.article.update({
+      where: { id },
+      data: updateArticleDto,
+    });
+  }
+
+  remove(id: number) {
+    //This action removes a #${id} article;
+    return this.prisma.article.delete({ where: { id } });
+  }
+}
